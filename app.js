582399a91c2bf1dda7ba6708fae661206b1b3ced
noddycha <nadiger.chaitanya@gmail.com>
2019-08-03 14:01:07 +0530

    Adding server changes

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var app, server,
     host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000,
     root = path.resolve(__dirname, '.');
- 
+
+var http = require('http');
+    
 app = express();
 app.use(function(req, res, next) { console.log(req.url); next(); });
 app.use(express.static(root + '/public'));
-server = app.listen(port, host, serverStarted);
- 
-function serverStarted () {
-    console.log('Server started', host, port);
-    console.log('Root directory', root);
-    console.log('Press Ctrl+C to exit...\n');
-}
+
+http.createServer(app).listen(port, function () {
+  console.log(""); // for blank line for readability
+  console.log("Express server listening on port " + port);
+  console.log(""); // for blank line for readability
+  console.log("Server running successfully");
+  // only return message to register in Bot Framework if it is set to something other than your locally running instance
+});
